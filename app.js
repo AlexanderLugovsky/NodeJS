5ced7a6bf730de7008a9fcfcd62cfd7a10da9594
Alexandr Lugovskiy <73530174+AlexanderLugovsky@users.noreply.github.com>
2021-02-03 20:06:02 +0200

    Асинхронная запись файла

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const fs = require("fs");
  
-// асинхронное чтение
-fs.readFile("hello.txt", "utf8", 
-            function(error,data){
-                console.log("Асинхронное чтение файла");
-                if(error) throw error; // если возникла ошибка
-                console.log(data);  // выводим считанные данные
-});
+fs.writeFile("hello.txt", "Hello мир!", function(error){
  
-// синхронное чтение
-console.log("Синхронное чтение файла")
-let fileContent = fs.readFileSync("hello.txt", "utf8");
-console.log(fileContent);
+    if(error) throw error; // если возникла ошибка
+    console.log("Асинхронная запись файла завершена. Содержимое файла:");
+    let data = fs.readFileSync("hello.txt", "utf8");
+    console.log(data);  // выводим считанные данные
+});
