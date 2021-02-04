4cace3bf6d5a31b59fa7fb2a027b8df2079da7cb
Alexandr Lugovskiy <lugovskij@gmail.com>
2021-02-04 14:44:05 +0200

    События1

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const fs = require("fs");
+const util = require("util");
+const EventEmitter = require("events");
  
-fs.unlink("hello.txt", (err) => {
-    if (err) console.log(err); // если возникла ошибка    
-    else console.log("hello.txt was deleted");
-  });
+function User(){
+}
+util.inherits(User, EventEmitter);
+ 
+let eventName = "greet";
+User.prototype.sayHi = function(data){
+    this.emit(eventName, data);
+}
+let user = new User();
+// добавляем к объекту user обработку события "greet"
+user.on(eventName, function(data){
+    console.log(data);
+});
+ 
+user.sayHi("Мне нужна твоя одежда...");
