903694187212b3ddcc0aba5a33ffc056b5fd1644
Alexandr Lugovskiy <lugovskij@gmail.com>
2021-02-05 12:25:18 +0200

    Server2

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-var http = require("http");
- 
+const http = require("http");
+  
 http.createServer(function(request, response){
      
-    console.log("Url: " + request.url);
-    console.log("Тип запроса: " + request.method);
-    console.log("User-Agent: " + request.headers["user-agent"]);
-    console.log("Все заголовки");
-    console.log(request.headers);
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
      
+    if(request.url === "/home" || request.url === "/"){
+        response.write("<h2>Home</h2>");
+    }
+    else if(request.url == "/about"){
+        response.write("<h2>About</h2>");
+    }
+    else if(request.url == "/contact"){
+        response.write("<h2>Contacts</h2>");
+    }
+    else{
+        response.write("<h2>Not found</h2>");
+    }
     response.end();
 }).listen(3000);
